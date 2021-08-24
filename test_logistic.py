9634f62aa26c5f311e96a84923eeb0ccf72c8c39
ASPP Student <student@localhost>
2021-08-24 15:18:29 +0200

    fuzzy testing!

--- test_logistic.py
+++ test_logistic.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 from math import isclose
 from logistic import f, iterate_f
+import random
 
 @pytest.mark.parametrize("x,r,expected",[(0.1,2.2,0.198),
 					  (0.2,3.4,0.544),
@@ -19,5 +20,20 @@ def test_logistic_iterated(x, r, it, expected):
 	# When
 	result = iterate_f(it, x, r)
 	# Then
-	np.testing.assert_allclose(result, np.array(expected), rtol=1e-5)
+	np.testing.assert_allclose(result, np.array(expected), rtol=1e-3)
+
+def test_logistic_iterated():
+	# Given
+	r = 1.5
+	it = 50
+	expected = [1/3] * 100
+	random_x = np.random.rand(100)
+	converged_x = np.zeros(100)
+	# When
+	for i, x in enumerate(random_x):
+		result = iterate_f(it, x, r)
+		converged_x[i] = result[-1]
+	# Then
+	np.testing.assert_allclose(converged_x, expected, rtol=1e-3)
+
 
